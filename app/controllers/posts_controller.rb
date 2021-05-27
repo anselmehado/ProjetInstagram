823619fb06aa2082c91275a24f487e4d147b8b20
hado anselme <anselmehado85@yahoo.com>
2021-05-27 13:22:20 +0100

    ok

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -75,7 +75,7 @@ class PostsController < ApplicationController
    @post = Post.find(params[:id])
  end
  def post_params
-   params.require(:post).permit(:name, :email, :content)
+   params.require(:post).permit(:content,:avatar)
  end
 
 
