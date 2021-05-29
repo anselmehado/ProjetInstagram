dcac37d8182622e66a1a7478b6c048a2943a3ec3
hado anselme <anselmehado85@yahoo.com>
2021-05-29 08:33:37 +0100

    update

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ module ProjetInstag
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.time_zone = 'Lisbon'
+    config.active_record.default_timezone = :local
   end
 end
